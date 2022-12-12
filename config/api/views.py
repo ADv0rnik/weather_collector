f3abaf8a504d3731f6c64bf50939a137dab00c5a
Alex Dvornik <aadvornik@gmail.com>
2022-12-12 22:23:16 +0300

    set up logger for the app

--- config/api/views.py
+++ config/api/views.py
@@ -1,8 +1,12 @@
+import logging
+
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 
 from api.serializer import WeatherSerializer
 from collector.models import Weather
+
+logger = logging.getLogger('collector')
 
 
 class WeatherViewSet(viewsets.ReadOnlyModelViewSet):
@@ -12,7 +16,7 @@ class WeatherViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         queryset = Weather.objects.all()
         region = self.request.query_params.get('region')
-        print(region)
+        logger.info(f"Get queryset for {region} ...")
         if region:
             queryset = Weather.objects.filter(region=region)
         return queryset
